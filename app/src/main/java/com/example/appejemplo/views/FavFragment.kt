3b3f4b73d2278cc4d98f5b0b1f9bc8bd85e97643
davidcl24 <davidcl.flor@gmail.com>
2025-02-19 20:58:44 +0100

    Favourite list almost implemented

--- app/src/main/java/com/example/appejemplo/views/FavFragment.kt
+++ app/src/main/java/com/example/appejemplo/views/FavFragment.kt
@@ -1,19 +1,28 @@
 package com.example.appejemplo.views
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.appejemplo.App
+import com.example.appejemplo.adapters.AnimeAdapter
+import com.example.appejemplo.adapters.AnimeOnClickDetail
 import com.example.appejemplo.databinding.FragmentFavBinding
 import com.example.appejemplo.viewmodels.FavViewModel
 import com.example.appejemplo.viewmodels.FavViewModelFactory
 
 
-class FavFragment : Fragment() {
+class FavFragment : Fragment(), AnimeOnClickDetail {
     private lateinit var binding: FragmentFavBinding
+    private lateinit var mAdapter: AnimeAdapter
+    private lateinit var mLayoutManager: GridLayoutManager
+
     private val viewModel: FavViewModel by viewModels {
         FavViewModelFactory(App.db.animeDao())
     }
@@ -33,10 +42,33 @@ class FavFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+        onClickBackHandle()
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun setupRecyclerView() {
+        mAdapter = AnimeAdapter(mutableListOf(), this)
+        mLayoutManager = GridLayoutManager(requireContext(), 2)
+        binding.recyclerView.apply {
+            layoutManager = mLayoutManager
+            adapter = mAdapter
+        }
     }
 
     fun deleteContact(position: Int) {
         TODO("Not yet implemented")
+    }
+
+    override fun onClickAnime(id: Int) {
+        val action = FavFragmentDirections.actionFavFragmentToAnimeDetailFragment(id)
+        findNavController().navigate(action)
+    }
+
+    private fun onClickBackHandle() {
+        requireActivity().onBackPressedDispatcher.addCallback {
+            findNavController().navigateUp()
+        }
     }
 
 }
